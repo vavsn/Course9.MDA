024a93ad848b4cc196e884c678c9878cb209f0ba
vavsn <vavsn@mail.ru>
2022-10-19 11:31:58 +0700

    Lesson 2. Homework Send messaging over RabbitMQ

---
+++ Restaurant/Restaurant.Notification/Worker.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using Messaging;
+using Microsoft.Extensions.Hosting;
+
+namespace Restaurant.Notification
+{
+    public class Worker : BackgroundService
+    {
+        private readonly Consumer _consumer;
+
+        public Worker()
+        {
+            //важно чтобы имя очереди совпадало
+            _consumer = new Consumer("BookingNotification", "localhost");
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _consumer.Receive((sender, args) =>
+            {
+                var body = args.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body); //декодируем
+                Console.WriteLine(" [x] Received {0}", message);
+            });
+        }
+    }
+}
